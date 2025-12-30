b4e288fab825119d90b255e7cf00643e5899a093
Emad Al-Khorasani <ealkhorasani@gmail.com>
2025-12-31 01:27:53 +0800

    Update nexus_pipeline.py

--- dags/nexus_pipeline.py
+++ dags/nexus_pipeline.py
@@ -2,14 +2,18 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
+# Define the DAG and its core properties
 with DAG(
     dag_id='nexus_pipeline_v1',
     start_date=datetime(2023, 1, 1),
-    schedule_interval=None,
-    catchup=False
+   schedule_interval=None,  # Triggered manually
+    catchup=False            # Do not run missed past tasks
 ) as dag:
 
+     # Task 1: Simulate data processing using Spark
+    # We use BashOperator to execute system commands directly inside the container
     process_data = BashOperator(
         task_id='spark_process_task',
-        bash_command='echo "Running Spark Task..." && sleep 5 && echo "Success!"'
+        bash_command='echo "Starting Spark Job..." && sleep 5 && echo "Data Transformation Completed Successfully!"'
+
     )
